f2bf7e96f40186fbaed51427ce5bfd78abee1a43
Lawrence Lin <lawwwlin@gmail.com>
2021-07-01 04:35:18 +0000

    attempted to check diagonal, but not finished

--- wordsearch.js
+++ wordsearch.js
@@ -14,14 +14,63 @@ const transpose = function(matrix) {
 const checkMatrix = (matrix, word) => {
   for (const row of matrix) {
     if (row.includes(word)) {
-      return true
+      return true;
     }
   }
+  return false;
 };
+
+const traverse = (row, col, matrix, word) => {
+  const firstLetter = word.charAt(0);
+  const height = matrix.length;
+  const width = matrix[row].length;
+  const diagonal = width - row;
+  const array = [[]];
+  if (row >= height || col >= width || word.length > diagonal) {
+    return array;
+  }
+  if (firstLetter === matrix[row][col]) { // if first letter matches, 
+    console.log(`diagonalChecking ${matrix} starting [${row}][${col}], for ${word}`)
+    array.push(diagonalCheck(row, col, matrix, word, []));
+  } else { // first letter doesn't match, look for match
+    if (col === width - 1) {
+      console.log("traverse recusion" + row+1 + " " + col+1 + " , to next row: " + row+1 + " " + col)
+      traverse(row + 1, 0, matrix, word);
+    } else {
+      console.log("traverse recusion" + row+1 + " " + col+1 + " , to next row: " + row + " " + col+1)
+      traverse(row, col + 1, matrix, word);
+    }
+  }
+  if (col === width - 1) {
+    console.log("traverse recusion" + row+1 + " " + col+1 + " , to next row: " + row+1 + " " + col)
+    traverse(row + 1, 0, matrix, word);
+  } else {
+    console.log("traverse recusion" + row+1 + " " + col+1 + " , to next row: " + row + " " + col+1)
+    traverse(row, col + 1, matrix, word);
+  }
+  return array;
+};
+
+const diagonalCheck = (row, col, matrix, word, arr) => {
+  if (matrix[row][col]) { // element exists
+    if (word.charAt(0) === matrix[row][col] && word.length === 1) {
+      // console.log(`diagonal check base case, [row][col] ${row}${col}, matrix: ${matrix[row][col]}, word: ${word}`);
+      arr.push(word);
+    } else if (word.charAt(0) === matrix[row][col]) {
+      // console.log(`diagonal check, [row][col] ${row}${col}, matrix: ${matrix[row][col]}, word: ${word}`);
+      arr.push(word);
+      arr.concat(diagonalCheck(row + 1, col + 1, matrix, word.substr(1), arr));
+    } else {
+      return arr;
+    }
+  }
+  return arr;
+}
 
 const wordSearch = (letters, word) => { 
   const horizontalJoin = letters.map(ls => ls.join(''));
   const rHorizontalJoin = letters.map(ls => ls.reverse().join(''));
+  letters.map(ls => ls.reverse());
   const transposed = transpose(letters);
   const verticalJoin = transposed.map(ls => ls.join(''));
   const rVerticalJoin = transposed.map(ls => ls.reverse().join(''));
@@ -29,7 +78,10 @@ const wordSearch = (letters, word) => {
   if (checkMatrix(verticalJoin, word)) return true;
   if (checkMatrix(rHorizontalJoin, word)) return true;
   if (checkMatrix(rVerticalJoin, word)) return true;
-  return false;
+  // const diagonalArrs = traverse(0, 0, letters, word);
+  // console.log(diagonalArrs);
+  // if (checkMatrix(diagonalArrs, word)) return true;
+return false;
 };
 
 module.exports = wordSearch
